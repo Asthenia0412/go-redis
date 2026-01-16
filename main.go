51e2117cfeeacb6a0dbf8848006294485073554d
yancy.xiao <yancy@MacBook-Air.local>
2026-01-16 11:13:04 +0800

    基于Golang实现了链接的监听But尚未实现RESP的解析

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	// 1. 监听端口6379
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Error Listening", err)
+		os.Exit(1)
 	}
+	defer listener.Close()
+	fmt.Println("Redis Server已经启动成功了 端口是6379")
+	// 2.接收客户端连接
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("接收错误！ ", err)
+		}
+		// 3.启动goroutine处理链接
+
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("新的链接建立了！", conn.RemoteAddr())
 }
